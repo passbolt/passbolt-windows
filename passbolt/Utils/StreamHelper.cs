2d483eeb18cc3e3b40448add3587705a84503215
Pierre Colart <pierre@passbolt.com>
2023-05-12 07:23:19 +0000

    PB-24569 : As a desktop application I should not be blocked by CORS when calling API

---
+++ passbolt/Utils/StreamHelper.cs
@@ -0,0 +1,39 @@
+﻿/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) 2023 Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2023 Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         0.0.1
+ */
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace passbolt.Utils
+{
+    internal static class StreamHelper
+    {
+        /// <summary>
+        /// Transform stream to random access stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public async static Task <IRandomAccessStream> ConvertStreamToAccessStream(Stream stream)
+        {
+            var buffer = new byte[stream.Length];
+            await stream.ReadAsync(buffer, 0, buffer.Length);
+            var randomAccessStream = new InMemoryRandomAccessStream();
+            await randomAccessStream.WriteAsync(buffer.AsBuffer());
+            return randomAccessStream;
+        }
+    }
+}
