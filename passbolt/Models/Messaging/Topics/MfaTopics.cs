d4af97c2c9f75367765c4e370c696ce53fd71ecf
pierre <pierre@passbolt.com>
2023-09-29 06:55:55 +0200

    PB-27605 - As a sign-in user I can setup Yubikey as 2FA on the desktop application

--- passbolt/Models/Messaging/Topics/MfaTopics.cs
+++ passbolt/Models/Messaging/Topics/MfaTopics.cs
@@ -25,5 +25,6 @@ namespace passbolt.Models.Messaging.Topics
         public const string VERIFY_PROVIDER = "passbolt.mfa-setup.verify-provider";
         public const string REMOVE_TOTP = "passbolt.mfa-setup.remove-provider";
         public const string VERIFY_TOTP_CODE = "passbolt.mfa-setup.verify-totp-code";
+        public const string VERIFY_YUBIKEY_CODE = "passbolt.mfa-setup.verify-yubikey-code";
     }
 }
