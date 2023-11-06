71b12ecf22e0b2b716db6d680d2e1a5177523978
Pierre Colart <pierre@passbolt.com>
2023-11-06 15:10:15 +0000

    Feature/pb 28260 rbac hide unsupported feature from desktop app

---
+++ passbolt/Models/Rbac/ControlFunction.cs
@@ -0,0 +1,36 @@
+﻿/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) 2023 Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2023 Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         0.5.0
+ */
+
+using Newtonsoft.Json;
+using System;
+
+namespace passbolt.Models.Rbac
+{
+    public class ControlFunction
+    {
+        [JsonProperty("control_function")]
+        public string Control { get; set; }
+        [JsonProperty("foreign_id")]
+        public Guid ForeignId { get; set; }
+        [JsonProperty("foreign_model")]
+        public string ForeignModel { get; set; }
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+        [JsonProperty("role_id")]
+        public Guid RoleId { get; set; }
+        [JsonProperty("ui_action")]
+        public UiAction UiAction { get; set; }
+
+    }
+}
