b79657063aec9ed1f97dded5dfc8ec544dc2cf88
pierre <pierre@passbolt.com>
2026-03-17 14:49:09 +0100

    PB-50059: remove dialogs which are not supportable

--- passbolt/Webviews/Rendered/src/AppWorkspace.js
+++ passbolt/Webviews/Rendered/src/AppWorkspace.js
@@ -60,7 +60,6 @@ import {ResizableSidebarContextProvider} from "passbolt-styleguide/src/react-ext
 import SecretRevisionsSettingsContextProvider from "passbolt-styleguide/src/shared/context/SecretRevisionSettingsContext/SecretRevisionsSettingsContext";
 import RoleContextProvider from "passbolt-styleguide/src/react-extension/contexts/RoleContext";
 import ExportPoliciesSettingsContextProvider from "passbolt-styleguide/src/react-extension/contexts/ExportPoliciesSettingsContext";
-import HandleStatusCheck from "passbolt-styleguide/src/react-extension//components/HandleStatusCheck/HandleStatusCheck";
 
 /**
  * The passbolt application served by the desktop.
@@ -85,7 +84,7 @@ class AppWorkspace extends Component {
       <RenderedWebview port={this.props.port}>
         {this.isReady() && <ExtAppContextProvider port={this.props.port} storage={this.props.storage}>
           <AppContext.Consumer>
-            {appContext =>
+            {() =>
               <TranslationProvider loadingPath="https://rendered.dist/Rendered/dist/locales/{{lng}}/{{ns}}.json">
                 <RbacContextProvider>
                   <AccountRecoveryUserContextProvider accountRecoveryUserService={accountRecoveryUserService}>
@@ -111,11 +110,6 @@ class AppWorkspace extends Component {
 
                                           <Router>
                                             <NavigationContextProvider>
-                                              {appContext.loggedInUser &&
-                                              (appContext.siteSettings.canIUse("accountRecovery") ||
-                                                appContext.siteSettings.canIUse("mfaPolicies")) && (
-                                                <HandleStatusCheck />
-                                              )}
                                               <Switch>
                                                 {/* Passwords workspace */}
                                                 <Route path={[
